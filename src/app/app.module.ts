b5b928729cc77d1c44e78df79e46d8ada03b6083
Renzo <renzoperezquispe97@gmail.com>
2024-10-01 19:18:10 -0500

    Fin de Sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { CounterModule } from './counter/counter.module';
+import { HerosModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HerosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
